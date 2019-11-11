4b96ac30604f193ee00b56d6e05cfa1d7a1938aa
ArtemFrantsiian <artemfrantsiian@gmail.com>
2019-11-11 18:41:22 +0200

    [REG-66] Setting up Typescript and linter

---
+++ packages/core/dist/index.d.ts
@@ -0,0 +1,4 @@
+declare const _default: {
+    add: (left: number, right: number) => number;
+};
+export = _default;
---
+++ packages/core/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var add = function (left, right) {
+    return left + right;
+};
+module.exports = {
+    add: add
+};
+//# sourceMappingURL=index.js.map
