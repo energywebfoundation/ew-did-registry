7c1623d22610b6648e858d78d8db4b16c19569e0
Raid5594 <52794079+Raid5594@users.noreply.github.com>
2019-11-15 12:53:58 +0200

    [REG-68] add script to generate config file, automatic generation with … (#8)
    * REG-68 add script to generate config file, automatic generation with gulp
    
    * REG-68 remove errors
    
    * REG-68 fixed spacing
    
    * REG-68 a few more errors fixed

---
+++ .dependabot/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const config = `version: 1
+
+update_configs:
+`;
+
+const packageTemplate = (dir) => `
+  - package_manager: "javascript"
+    directory: "${dir}"
+    update_schedule: "live"
+    default_reviewers:
+      - ArtemFrantsiian
+      - ni3gavhane
+`;
+
+function configDependabot() {
+    const directories = [
+        '/',
+        ...fs.readdirSync(path.join(__dirname, '../packages'))
+    ];
+
+    const stream = fs.createWriteStream('./.dependabot/config.yml');
+
+    stream.write(config);
+
+    directories.forEach((directory, index) => {
+        const path = index ? `/packages/${directory}` : directory;
+        stream.write(packageTemplate(path));
+    });
+
+    stream.end();
+};
+
+module.exports = configDependabot;
