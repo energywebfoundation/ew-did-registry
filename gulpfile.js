5639ec5b8e0154476514834ba4a17c48b52f0959
Artem Frantsiian <35114937+ArtemFrantsiian@users.noreply.github.com>
2019-11-18 11:56:30 +0300

    Feature/typedoc (#10)
    * [REG-90] add auto doc generation
    
    * [REG-90] add to the default

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
-const { series, src, dest } = require('gulp');
 const path = require('path');
-const del = require('del');
+
+const { series, src, dest } = require('gulp');
 const replace = require('gulp-replace');
 const rename = require('gulp-rename');
 const streamify = require('gulp-streamify');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const typedoc = require('gulp-typedoc');
+
+const del = require('del');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const tsify = require('tsify');
+
 const lernaJSON = require('./lerna.json');
 const configDependabot = require('./.dependabot');
 
@@ -28,7 +32,7 @@ const uglifyOptions = {
     dead_code: true,
     drop_debugger: true,
     global_defs: {
-      "DEBUG": false
+      DEBUG: false
     }
   }
 };
@@ -38,14 +42,9 @@ function clean(done) {
   done();
 }
 
-function generateDependabotConfig(done) {
-  configDependabot();
-  done();
-}
-
 function version() {
   if (!lernaJSON.version) {
-    throw new Error("version property is missing from lerna.json");
+    throw new Error('version property is missing from lerna.json');
   }
 
   const version = lernaJSON.version;
@@ -57,11 +56,11 @@ function version() {
   return src(glob, {
       base: './'
   })
-      .pipe(replace(jsonPattern, '"version": "' + version + '"'))
+      .pipe(replace(jsonPattern, `"version": "${version}"`))
       .pipe(dest('./'));
 }
 
-function bundling(package) {
+function bundling(pckg) {
   return browserify({
     basedir: '.',
     debug: true,
@@ -69,11 +68,11 @@ function bundling(package) {
     insertGlobalVars: false,
     detectGlobals: true,
     bundleExternal: true,
-    entries: [ path.join(package.src, 'src/index.ts') ],
+    entries: [ path.join(pckg.src, 'src/index.ts') ],
     cache: {},
     packageCache: {}
   })
-    .plugin(tsify, { project: package.src })
+    .plugin(tsify, { project: pckg.src })
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(buffer())
@@ -82,25 +81,59 @@ function bundling(package) {
       compact: true,
       presets: [
         ['@babel/env', {
-          "modules": false
+          'modules': false
         }]
       ]
     })))
     .pipe(streamify(uglify(uglifyOptions)))
-    .pipe(rename(package.fileName + '.min.js'))
+    .pipe(rename(pckg.fileName + '.min.js'))
     .pipe(sourcemaps.write('./'))
     .pipe(dest(DEST));
 }
 
-function bundleAll(callback) {
-  packages.forEach(function (pckg) {
+function bundleAll(done) {
+  packages.forEach((pckg) => {
     bundling(pckg);
   });
-  callback()
+  done()
+}
+
+function generateDependabotConfig(done) {
+  configDependabot();
+  done();
+}
+
+function buildDocs() {
+  return src(['packages/**/*.ts', '!./packages/**/node_modules/**/*.ts', '!./packages/**/*.d.ts'])
+    .pipe(typedoc({
+      // TypeScript options (see typescript docs)
+      module: 'commonjs',
+      target: 'es5',
+      includeDeclarations: false,
+      lib: [ 'lib.esnext.full.d.ts' ],
+
+      // TypeDoc options (see typedoc docs)
+      out: './docs',
+      exclude: '**/*+(e2e|spec).ts',
+      excludeExternals: true,
+      excludePrivate: true,
+      excludeProtected: true,
+      ignoreCompilerErrors: true,
+      moduleResolution: 'node',
+      preserveConstEnums: true,
+      skipLibCheck: true,
+      stripInternal: true,
+      suppressExcessPropertyErrors: true,
+      suppressImplicitAnyIndexErrors: true,
+      mode: 'file',
+      plugins: ['typedoc-plugin-markdown']
+      // externalPattern: '**/node_modules/**',
+    }));
 }
 
 exports.clean = clean;
 exports.version = version;
 exports.bundleAll = bundleAll;
-exports.dependabot = generateDependabotConfig;
-exports.default = series(clean, version, bundleAll);
+exports.generateDependabotConfig = generateDependabotConfig;
+exports.buildDocs = buildDocs;
+exports.default = series(clean, version, bundleAll, buildDocs);
