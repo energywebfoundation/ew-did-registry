25c5989ca067f765ea0a76aad9c7c56d86c738a1
Raid Ateir <ateirraid@gmail.com>
2019-11-19 16:23:59 +0200

    REG-69 bug fix

---
+++ dist/did-registry/src/index.d.ts
@@ -0,0 +1,2 @@
+declare const add: (left: number, right: number) => number;
+export { add, };
---
+++ dist/keys/src/index.d.ts
@@ -0,0 +1,3 @@
+import { IKeys } from './interface';
+declare const add: (left: number, right: number) => number;
+export { add, IKeys, };
