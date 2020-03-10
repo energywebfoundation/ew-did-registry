9b6e652c74c5de2b4f099719a966d0de4b7d978c
JGiter <dimaadipmi@gmail.com>
2020-03-10 11:26:18 +0200

    REG-198 implement sendTransaction

--- packages/proxyIdentity/contracts/proxyIdentity.sol
+++ packages/proxyIdentity/contracts/proxyIdentity.sol
@@ -5,8 +5,15 @@ contract ProxyIdentity {
 
     address public owner;
 
-    event TransactionSend(bytes data, address to, bool success);
+    event TransactionSend(
+        bytes data,
+        address to,
+        bool success,
+        bytes4 sig,
+        uint256 len
+    );
     event IdentityOwner(address identity, bool success);
+    event ChangeOwner(address to, address first, address second);
 
     constructor() public {
         owner = msg.sender;
@@ -30,66 +37,20 @@ contract ProxyIdentity {
         _;
     }
 
-    function identityOwner(address identity, address to)
-        public
-        returns (address result)
-    {
+    function sendTransaction(bytes calldata _data, address to) external _owner {
         bool success;
-        bytes4 sig = bytes4(keccak256("identityOwner(address)"));
-        // solium-disable-next-line security/no-inline-assembly
-        assembly {
-            let x := mload(0x40)
-            mstore(x, sig)
-            mstore(add(x, 0x04), identity)
-            success := call(5000, to, 0, x, 0x24, x, 0x20)
-            if eq(success, 0) {
-                revert(0, 0)
-            }
-            result := mload(x)
-            mstore(0x40, add(x, 0x44))
-        }
-        emit IdentityOwner(identity, success);
-    }
-
-    function execute(
-        bytes calldata _data,
-        address to,
-        address first,
-        address second
-    ) external _owner {
-        bool success;
+        bytes memory data = _data;
+        uint256 len = data.length;
         bytes4 sig = bytes4(keccak256("changeOwner(address,address)"));
-        // string memory first = "0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0";
-        // string memory second = "0xB8427ADa6e4F5eCF98d6981Ae0A5B9CA3BB7B4c6";
-        // address _owner = owner;
-        // bytes memory data = _data;
-        // address _identityOwner; //call to identityOwner()
-        // uint256 len = data.length;
         // solium-disable-next-line security/no-inline-assembly
         assembly {
-            let x := mload(0x40) // Find empty storage location using "free memory pointer"
-            mstore(x, sig)
-            mstore(add(x, 0x04), first)
-            mstore(add(x, 0x24), second)
-            success := call(5000, to, 0, x, 0x44, 0, 0)
+            success := call(100000, to, 0, add(data, 0x20), len, 0, 0)
             if eq(success, 0) {
                 revert(0, 0)
             }
-            mstore(0x40, add(x, 0x64)) // Set storage pointer to new space
         }
-        emit TransactionSend(_data, to, success);
+        emit TransactionSend(_data, to, success, sig, len);
     }
 
-    // copied from GnosisSafe
-    // https://github.com/gnosis/safe-contracts/blob/v0.0.2-alpha/contracts/base/Executor.sol
-    function _sendTransaction(bytes memory data, address to)
-        internal
-        returns (bool success)
-    {
-        // solium-disable-next-line security/no-inline-assembly
-        assembly {
-            success := call(gas, to, 0, add(data, 0x20), mload(data), 0, 0)
-        }
-    }
-
+    function() external payable {}
 }
--- packages/proxyIdentity/test/proxy.test.ts
+++ packages/proxyIdentity/test/proxy.test.ts
@@ -1,53 +1,59 @@
-import { Contract, providers, ContractFactory, Signer } from 'ethers';
+import { Contract, providers, ContractFactory } from 'ethers';
 import Web3 from 'web3';
+import { expect } from 'chai';
 import { abi, bytecode } from '../build/contracts/ProxyIdentity.json';
 import { ethrReg } from '../../did-resolver';
 import { Keys } from '../../keys';
 
+const web3 = new Web3('http://localhost:8544');
+const { encodeFunctionCall, encodeParameter } = web3.eth.abi;
 const { JsonRpcProvider } = providers;
 const { abi: abi1056, bytecode: bytecode1056 } = ethrReg;
 
-describe.skip('[PROXY IDENTITY PACKAGE/PROXY]', () => {
+describe('[PROXY IDENTITY PACKAGE/PROXY CONTRACT]', function () {
+  this.timeout(0);
   let proxy: Contract;
   let erc1056: Contract;
   const provider = new JsonRpcProvider('http://localhost:8544');
-  const creator: Signer = provider.getSigner(0);
+  const creator: providers.JsonRpcSigner = provider.getSigner(0);
   const proxyFactory = new ContractFactory(abi, bytecode, creator);
   const erc1056Factory = new ContractFactory(abi1056, bytecode1056, creator);
-  const web3 = new Web3('http://localhost:8544');
 
-  before(async () => {
-    const creatorAddress = await creator.getAddress();
-    console.log('>> Creator:', creatorAddress);
+  beforeEach(async () => {
     proxy = await (await proxyFactory.deploy()).deployed();
-    console.log('>> Proxy:', proxy.address);
     erc1056 = await (await erc1056Factory.deploy()).deployed();
-    console.log('>> ERC1056:', erc1056.address);
+    await creator.sendTransaction({ value: 1000000000, to: proxy.address, gasLimit: 100000 });
   });
 
-  it('proxy owner must be proxy creator', async () => {
-    proxy.once('IdentityOwner', (identty, success) => console.log('identityOwner success:', success));
-    // const ownersAbi = ethrReg.abi.find((f) => f.name === 'owners');
-    // const data: string = web3.eth.abi.encodeFunctionCall(ownersAbi as any, [proxy.address]);
-    const tx = await proxy.identityOwner(proxy.address, erc1056.address);
-    await tx.wait();
-  });
-
-  it('changeOwner() should emit DIDOwnerChanged on ERC1056', async () => {
-    const newOwnerAddress = new Keys().getAddress();
-    console.log('new owner address;', newOwnerAddress);
-    erc1056.once('DIDOwnerChanged', (id, owner) => console.log(`>>> owner of ${id} changed to ${owner}`));
-    proxy.once('TransactionSend', async (data, address, success) => {
-      console.log(`>>> send transaction ${data} to ${address} with ${success}`);
-      const proxyOwner = await erc1056.identityOwner(proxy.address);
-      console.log('owner after change:', proxyOwner);
+  it('sendTransaction with encoded changeOwner() calldata should emit DIDOwnerChanged on ERC1056', (done) => {
+    erc1056.on('DIDOwnerChanged', (identity, owner, previousChange) => {
+      erc1056.removeAllListeners('DIDOwnerChanged');
+      done();
     });
-    const changeOwnerAbi = ethrReg.abi.find((f) => f.name === 'changeOwner');
-    const proxyOwner = await erc1056.identityOwner(proxy.address);
-    console.log('owner before change:', proxyOwner);
-    const data: string = web3.eth.abi.encodeFunctionCall(changeOwnerAbi as any, [proxy.address, newOwnerAddress]);
-    const sig = web3.eth.abi.encodeFunctionSignature(changeOwnerAbi as any);
-    console.log('change owner signature:', sig);
-    await (await proxy.execute(data, erc1056.address, await creator.getAddress(), newOwnerAddress)).wait();
+    const newOwnerAddress = new Keys().getAddress();
+    erc1056.identityOwner(proxy.address)
+      .then((identityOwner: string) => {
+        expect(identityOwner).equal(proxy.address);
+        const changeOwnerAbi: any = ethrReg.abi.find((f) => f.name === 'changeOwner');
+        const data: string = web3.eth.abi.encodeFunctionCall(changeOwnerAbi, [proxy.address, newOwnerAddress]);
+        return proxy.sendTransaction(data, erc1056.address);
+      })
+      .then((tx: any) => tx.wait())
+      .then(() => erc1056.identityOwner(proxy.address))
+      .then((identityOwner: string) => {
+        expect(identityOwner).equal(newOwnerAddress);
+      });
+  });
+
+  it('sendTransaction with encoded setAttribute() calldata should emit DIDAttributeChanged on ERC1056', (done) => {
+    erc1056.on('DIDAttributeChanged', (id, n, v, validTo, previouse) => {
+      erc1056.removeAllListeners('DIDAttributeChanged');
+      done();
+    });
+    const setAttributeAbi: any = ethrReg.abi.find((f) => f.name === 'setAttribute');
+    const attribute = web3.eth.abi.encodeParameter('bytes32', web3.utils.asciiToHex('name'));
+    const value = web3.eth.abi.encodeParameter('bytes', web3.utils.asciiToHex('John'));
+    const data: string = web3.eth.abi.encodeFunctionCall(setAttributeAbi, [proxy.address, attribute, value, '1000']);
+    proxy.sendTransaction(data, erc1056.address).then((tx: any) => tx.wait());
   });
 });
