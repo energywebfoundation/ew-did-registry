47453ef12cfbc418464f4fb1c7f4678773b8296c
JGiter <44746858+JGiter@users.noreply.github.com>
2020-03-15 21:16:15 +0200

    feature/REG-199-PROXY-FACTORY-CONTRACT (#144)
    * feature/REG-194-PERFORMANCE add readAttribute
    
    * feature/REG-194 include readAttribute to IResolver
    
    * feature/REG-194 fix public key to jwt.verify with 0x
    
    * feature/REG-194 fix ethereum address pattern
    
    * feature/REG-194 clean resolver
    
    * REG-198 impelment identityOwner
    
    * REG-198 implement sendTransaction
    
    * feature/REG-198 implement sendSignedTransaction
    
    * feature/REG-198 implement changeOwner
    
    * feature/REG-199 implement create
    
    * feature/REG-199 test create
    
    * feature/REG-199 send value through proxy
    
    * feature/REG-199 test send signed tx from non-owner
    
    * feature/REG-199 change revert from assembly on bool
    
    * feature/REG-198 add owner to revocation agents
    
    * feature/REG-198 track build in proxyIdentity
    
    * cleanup smart contract and tests
    
    * Proxy factory as creator, merge changes from REG-198
    
    * make compile pass
    
    * reset script and ProxyFactory.json
    
    Co-authored-by: mroon <micha.roon@energyweb.org>
    Co-authored-by: Dmitry Fesenko <dmitry.fesenko@482.solutions>

---
+++ packages/proxyIdentity/contracts/proxyIdentity.sol
@@ -0,0 +1,139 @@
+pragma solidity 0.5.8;
+
+interface IERC1056 {
+    function addDelegate(
+        address identity,
+        bytes32 delegateType,
+        address delegate,
+        uint256 validity
+    ) external;
+
+    function revokeDelegate(
+        address identity,
+        bytes32 delegateType,
+        address delegate
+    ) external;
+}
+
+contract ProxyIdentity {
+    address public creator;
+    address public owner;
+    address erc1056;
+    mapping(address => bool) recoveryAgents;
+    uint256 defaultValidity = 10 * 60 * 1000;
+
+    event TransactionSent(bytes data, address to, uint256 value);
+    event OwnerChanged(address identity, address prev, address next);
+    event RecoveryAgentAdded(address agent);
+    event RecoveryAgentRemoved(address agent);
+
+    constructor(address _erc1056) public payable {
+        erc1056 = _erc1056;
+        creator = msg.sender;
+        _changeOwner(msg.sender);
+    }
+
+    modifier _owner() {
+        require(msg.sender == owner, "Only owner allowed");
+        _;
+    }
+
+    modifier _recoveryAgent() {
+        require(
+            recoveryAgents[msg.sender],
+            "Only recovery agent can change the owner"
+        );
+        _;
+    }
+
+    function sendTransaction(bytes memory _data, address to, uint256 value)
+        public
+        payable
+        _owner
+    {
+        require(
+            _sendTransaction(_data, to, value),
+            "Can't send transaction"
+        );
+    }
+
+    function _sendTransaction(bytes memory _data, address to, uint256 value)
+        internal
+        returns (bool success)
+    {
+        bytes memory data = _data;
+        uint256 len = data.length;
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := call(gas, to, value, add(data, 0x20), len, 0, 0)
+        }
+        emit TransactionSent(_data, to, value);
+    }
+
+    function sendSignedTransaction(
+        bytes memory data,
+        address to,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public payable {
+        bytes32 digest = keccak256(data);
+        bytes32 hash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", digest)
+        );
+        address signer = ecrecover(hash, v, r, s);
+        require(owner == signer, "Signature is not valid");
+        require(
+            _sendTransaction(data, to, msg.value),
+            "Can't send transaction"
+        );
+    }
+
+    function addRecoveryAgent(address agent) public _owner {
+        _addRecoveryAgent(agent);
+    }
+
+    function _addRecoveryAgent(address agent) internal {
+        recoveryAgents[agent] = true;
+        emit RecoveryAgentAdded(agent);
+    }
+
+    function removeRecoveryAgent(address agent) public _recoveryAgent {
+        recoveryAgents[agent] = false;
+        emit RecoveryAgentRemoved(agent);
+    }
+
+    /**
+    * Used by the proxy factory to make sender the owner
+     */
+    function changeOwner(address newOwner) public  _recoveryAgent{
+        _changeOwner(newOwner);
+    }
+
+    function _changeOwner(address newOwner) internal {
+        address oldOwner = owner;
+        if (owner != address(0x0)) {
+            removeRecoveryAgent(owner);
+            _revokeDelegate(owner);
+        }
+        owner = newOwner;
+        _addRecoveryAgent(newOwner);
+        _addDelegate(newOwner);
+        emit OwnerChanged(address(this), oldOwner, newOwner);
+    }
+
+    function _addDelegate(address delegate) internal {
+        IERC1056(erc1056).addDelegate(
+            address(this),
+            "sigAuth",
+            delegate,
+            defaultValidity
+        );
+    }
+
+    function _revokeDelegate(address delegate) internal {
+        IERC1056(erc1056).revokeDelegate(address(this), "sigAuth", delegate);
+    }
+
+    function() external payable {}
+}
---
+++ packages/proxyIdentity/test/proxy.factory.test.ts
@@ -0,0 +1,49 @@
+import { Contract, providers, ContractFactory } from 'ethers';
+import { JsonRpcProvider } from 'ethers/providers';
+import { expect } from 'chai';
+import { ethrReg } from '../../did-ethr-resolver';
+import { abi as proxyAbi } from '../build/contracts/ProxyIdentity.json';
+import { abi as proxyFactoryAbi, bytecode as proxyFactoryBytecode } from '../build/contracts/ProxyFactory.json';
+
+const { abi: abi1056, bytecode: bytecode1056 } = ethrReg;
+
+describe('[PROXY IDENTITY PACKAGE/PROXY FACTORY CONTRACT]', function () {
+  this.timeout(0);
+  let erc1056: Contract;
+  let proxyFactory: Contract;
+  const provider = new JsonRpcProvider('http://localhost:8544');
+  const deployer: providers.JsonRpcSigner = provider.getSigner(0);
+  let deployerAddress: string;
+  const proxyFactoryCreator = new ContractFactory(proxyFactoryAbi, proxyFactoryBytecode, deployer);
+  const erc1056Factory = new ContractFactory(abi1056, bytecode1056, deployer);
+
+  beforeEach(async () => {
+    deployerAddress = await deployer.getAddress();
+    erc1056 = await (await erc1056Factory.deploy()).deployed();
+    proxyFactory = await (await proxyFactoryCreator.deploy(erc1056.address, { value: 1E15 })).deployed();
+  });
+
+  it('create() should set sender as owner of created proxy and proxyFactory as creator', (done) => {
+    proxyFactory.on('ProxyCreated', (proxyAddress: string) => {
+      // console.log(`>>> proxy created on ${proxyAddress}`);
+      proxyFactory.removeAllListeners('ProxyCreated');
+      const proxy = new Contract(proxyAddress, proxyAbi, deployer);
+      proxy.owner()
+          .then((proxyOwner: string) => {
+            expect(proxyOwner).equal(deployerAddress);
+            return proxy.creator()
+          })
+          .then((creator: string) => {
+            expect(creator).equal(proxyFactory.address);
+            done();
+          })
+          .catch((e: Error) => {
+            expect.fail(e.message);
+          })
+    });
+    proxyFactory.create({value: 1E15})
+        .then((tx: any) => {
+          tx.wait();
+        });
+  });
+});
