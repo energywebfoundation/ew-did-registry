d10d7df7db12628593ccdbee400f39b73a773e2c
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-04-09 15:50:33 +0300

    bugfix/REG-264 add methods

--- packages/did-document/test/did-document-lite.test.ts
+++ packages/did-document/test/did-document-lite.test.ts
@@ -1,62 +1,58 @@
 import { expect } from 'chai';
-import { Attributes } from '../src/models';
-
-import { Resolver } from '@ew-did-registry/did-ethr-resolver';
-
-import { DIDDocumentLite, IDIDDocumentLite } from '../src/lite';
+import { DIDAttribute, PubKeyType, Algorithms, Encoding, IOperator } from '@ew-did-registry/did-resolver-interface';
+import { Operator } from '@ew-did-registry/did-ethr-resolver';
+import { Methods } from '@ew-did-registry/did';
+import { Keys } from '@ew-did-registry/keys';
 import { getSettings } from '../../../tests/init-ganache';
+import { DIDDocumentLite, IDIDDocumentLite } from '../src';
 
-describe('[DID DOCUMENT LITE PACKAGE]', function() {
-  this.timeout(60000);
-  let didDocumentLite: IDIDDocumentLite;
-  let wrongDidDocumentLite: IDIDDocumentLite;
-  const did = 'did:ewc:0xe2e457aB987BEd9AbdEE9410FC985E46e28a3947';
-  const wrongId = 'did:ewc:0xe2e457aB987BEd9AbdEE9410FC985E46e28a394~';
+describe('[DID DOCUMENT LITE PACKAGE]', function () {
+  this.timeout(0);
+  const keys = new Keys();
+  const did = `did:${Methods.Erc1056}:${keys.getAddress()}`;
+  let operator: IOperator;
+  let docLite: IDIDDocumentLite;
 
   before(async () => {
-    const resolverSettings = await getSettings([]);
-    const resolver = new Resolver(resolverSettings);
-    didDocumentLite = new DIDDocumentLite(did, resolver);
-    wrongDidDocumentLite = new DIDDocumentLite(wrongId, resolver);
+    const resolverSettings = await getSettings([keys.getAddress()]);
+    operator = new Operator(keys, resolverSettings);
+    docLite = new DIDDocumentLite(did, operator);
   });
 
-  it('before first read did-document should be undefined', async () => {
-    expect(didDocumentLite.didDocument).to.be.undefined;
+  it('returned document should contain standard attributes', async () => {
+    const doc = await docLite.read();
+    expect(doc).include.keys('@context', 'id', 'publicKey', 'authentication', 'service');
   });
 
-  it('read should fetch did-document', async () => {
-    await didDocumentLite.read(Attributes.id);
-    expect(didDocumentLite.didDocument).include.keys('@context', 'id', 'publicKey', 'authentication', 'service');
+  it('document id should be equal to did', async () => {
+    const doc = await docLite.read();
+    const { id } = doc;
+    expect(id).to.equal(did);
   });
 
-  it('returned id should be equal to did', async () => {
-    const id = await didDocumentLite.read(Attributes.id);
-    expect(id).to.deep.equal(did);
+  it('readAttribute should find public key', async () => {
+    await operator.update(did, DIDAttribute.PublicKey, {
+      algo: Algorithms.ED25519,
+      type: PubKeyType.VerificationKey2018,
+      encoding: Encoding.HEX,
+      value: `0x${new Keys().publicKey}`,
+    });
+    const publicKey = await docLite.readAttribute({ publicKey: { type: 'Secp256k1VerificationKey' } });
+    expect(publicKey).to.be.not.undefined;
   });
 
-  it('returned publicKey should represent the owner', async () => {
-    const publicKey = await didDocumentLite.read(Attributes.publicKey, 'Secp256k1VerificationKey');
-    expect(publicKey).to.be.undefined;
+  it('readAttribute should find service endpoint', async () => {
+    const url = 'http://test.com';
+    await operator.update(did, DIDAttribute.ServicePoint, {
+      type: PubKeyType.VerificationKey2018,
+      value: url,
+    });
+    const service = await docLite.readAttribute({ serviceEndpoints: { serviceEndpoint: url } });
+    expect(service).to.be.not.undefined;
   });
 
-  it('invalid attribute should return undefined', async () => {
-    const wrongKey = await didDocumentLite.read('privateKey');
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    expect(wrongKey).to.be.undefined;
-  });
-
-  it('valid attribute with wrong type should return undefined', async () => {
-    const wrongKey = await didDocumentLite.read(Attributes.publicKey, 'ImpossibleKey');
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    expect(wrongKey).to.be.undefined;
-  });
-
-  it('invalid attribute with wrong type should return undefined', async () => {
-    const wrongKey = await didDocumentLite.read('privateKey', 'ImpossibleKey');
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    expect(wrongKey).to.be.undefined;
+  it('invalid attribute should return null', async () => {
+    const attr = await docLite.readAttribute({ serviceEndpoints: { nonexist: 'nonexist' } });
+    expect(attr).to.be.null;
   });
 });
