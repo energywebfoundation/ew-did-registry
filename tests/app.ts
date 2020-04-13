9d79d9404fc45de6ac86223f1acdc758f76680d5
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-04-13 11:57:03 +0300

    feature/REG-74 mock ipfs api in browser test

--- tests/app.ts
+++ tests/app.ts
@@ -3,17 +3,16 @@ import { Operator } from '../packages/did-ethr-resolver';
 import DIDRegistry from '../packages/did-registry';
 import { DidStore } from '../packages/did-ipfs-store';
 import { Methods } from '../packages/did';
-import { getSettings, spawnIpfsDaemon, shutDownIpfsDaemon } from '.';
+import { getSettings } from './init-ganache';
 
 export const createAndStoreClaim = async (): Promise<string> => {
   const keys = new Keys();
   const did = `did:${Methods.Erc1056}:${keys.getAddress()}`;
   const resolverSettings = await getSettings([]);
-  const store = new DidStore(await spawnIpfsDaemon());
+  const store = new DidStore('http://mockApi');
   const user = new DIDRegistry(keys, did, new Operator(keys, resolverSettings), store);
   const userClaims = user.claims.createClaimsUser();
   const claim = await userClaims.createPublicClaim({ name: 'John' });
   localStorage.setItem('EW-DID-CONFIG', claim);
-  await shutDownIpfsDaemon();
   return claim;
 };
