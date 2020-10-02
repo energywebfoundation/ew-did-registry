aac3894e8b4739332c4dedd929d33ba5a543e4d7
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-10-02 10:53:02 +0300

    move createProxy to proxyIdentity package

--- packages/proxyIdentity/index.ts
+++
@@ -1,4 +0,0 @@
-import * as proxyBuild from './build/contracts/ProxyIdentity.json';
-import * as proxyFactoryBuild from './build/contracts/ProxyFactory.json';
-
-export { proxyBuild, proxyFactoryBuild };
---
+++ packages/proxyIdentity/src/index.ts
@@ -0,0 +1,5 @@
+import * as proxyBuild from '../build/contracts/ProxyIdentity.json';
+import * as proxyFactoryBuild from '../build/contracts/ProxyFactory.json';
+
+export { proxyBuild, proxyFactoryBuild };
+export * from './proxyFacade';
