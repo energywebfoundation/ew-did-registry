b4d5b57e2ff388f47ec38a5ea1a611b7cf1bbaf4
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-10-19 10:59:01 +0300

    add proxy facade doc comment

--- packages/proxyIdentity/src/index.ts
+++ packages/proxyIdentity/src/index.ts
@@ -2,5 +2,6 @@ import * as proxyBuild from '../build/contracts/ProxyIdentity.json';
 import * as proxyFactoryBuild from '../build/contracts/ProxyFactory.json';
 import * as multiproxyBuild from '../build/contracts/ERC1155Multiproxy.json';
 
+export * as erc1056Build from '../constants/EthereumDIDRegistry';
 export { proxyBuild, proxyFactoryBuild, multiproxyBuild };
 export * from './proxyFacade';
