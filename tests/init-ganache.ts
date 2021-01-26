77759b681e9a4937097db34e6e01ba2a48a15f80
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-01-26 14:31:48 +0200

    feat: replace web3 in resolver

--- tests/init-ganache.ts
+++ tests/init-ganache.ts
@@ -1,21 +1,21 @@
-import { ContractFactory, ethers, Wallet } from 'ethers';
-import Web3 from 'web3';
-import { AsyncSendable } from 'ethers/providers';
+import {
+  ContractFactory, Wallet, providers, utils,
+} from 'ethers';
 import { ethrReg } from '../packages/did-ethr-resolver/src/constants';
 
+const { bigNumberify, hexlify } = utils;
+
 const GANACHE_PORT = 8544;
-const web3 = new Web3(`http://localhost:${GANACHE_PORT}`);
+const provider = new providers.JsonRpcProvider(`http://localhost:${GANACHE_PORT}`);
 
 export const deployRegistry = async (fillAccounts: Array<string>): Promise<string> => {
-  const accounts = await web3.eth.getAccounts();
+  const faucet = provider.getSigner(2);
 
-  await Promise.all(fillAccounts.map((acc) => web3.eth.sendTransaction({
-    from: accounts[2],
+  await Promise.all(fillAccounts.map((acc) => faucet.sendTransaction({
     to: acc,
-    value: '1000000000000000000',
+    value: hexlify(bigNumberify('1000000000000000000')),
   })));
 
-  const provider = new ethers.providers.Web3Provider(web3.currentProvider as AsyncSendable);
   const registryFactory = new ContractFactory(ethrReg.abi, ethrReg.bytecode,
     new Wallet('0x49b2e2b48cfc25fda1d1cbdb2197b83902142c6da502dcf1871c628ea524f11b', provider));
   const registry = await registryFactory.deploy();
