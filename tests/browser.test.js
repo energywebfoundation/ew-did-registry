961812abb3e09821e8875f776f810e648d7a65cd
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-03-29 13:30:01 +0300

    chore: remove karma

--- tests/browser.test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-
-describe('[EW-DID-REGISTRY]', () => {
-  it('Typescript application should be bundled to run in browser', async () => {
-    const createdClaim = await app.createAndStoreClaim();
-    const savedClaim = localStorage.getItem('EW-DID-CONFIG');
-    expect(createdClaim).equal(savedClaim);
-  });
-});
