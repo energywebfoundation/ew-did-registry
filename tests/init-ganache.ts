e6448a9a81c58d023beae78fd06bf988570577e1
nichonien <ashish.tripathi@energyweb.org>
2021-09-29 15:56:35 +0530

    feat/EDR-40/minor chode changes re testsuite

--- tests/init-ganache.ts
+++ tests/init-ganache.ts
@@ -12,6 +12,7 @@ const { hexlify } = utils;
 
 const GANACHE_PORT = 8544;
 const provider = new providers.JsonRpcProvider(`http://localhost:${GANACHE_PORT}`);
+const deployer = provider.getSigner(2);
 
 export const deployRegistry = async (fillAccounts: Array<string>): Promise<string> => {
   const faucet = provider.getSigner(2);
@@ -22,7 +23,7 @@ export const deployRegistry = async (fillAccounts: Array<string>): Promise<strin
   })));
 
   const registryFactory = new ContractFactory(ethrReg.abi, ethrReg.bytecode,
-    new Wallet('0x49b2e2b48cfc25fda1d1cbdb2197b83902142c6da502dcf1871c628ea524f11b', provider));
+    deployer);
   const registry = await registryFactory.deploy();
   // @TODO: deploy proxy factory
   return registry.address;
@@ -31,7 +32,7 @@ export const deployRegistry = async (fillAccounts: Array<string>): Promise<strin
 export const deployRevocationRegistry = async (): Promise<string> => {
 
   const registryFactory = new ContractFactory(RevocationRegistryOffChainAbi, RevocationRegistryOffChainByteCode,
-    new Wallet('0x49b2e2b48cfc25fda1d1cbdb2197b83902142c6da502dcf1871c628ea524f11b', provider));
+    deployer);
   const revocationRegistry = await registryFactory.deploy();
   return revocationRegistry.address;
 };
