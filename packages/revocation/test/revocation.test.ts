744f6a4f798633080f83837f69257bd1e2c5ec9d
nichonien <ashish.tripathi@energyweb.org>
2021-10-01 18:57:00 +0530

    feat/EDR-40/updated test cases wrt to lib update

--- packages/revocation/test/revocation.test.ts
+++ packages/revocation/test/revocation.test.ts
@@ -1,22 +1,16 @@
-import chai, { expect , assert } from 'chai';
+import { expect } from 'chai';
 import { providers } from 'ethers';
 import { Keys } from '@ew-did-registry/keys';
-import { Methods } from '@ew-did-registry/did';
 import { deployRevocationRegistry } from '../../../tests';
 import { ProviderSettings, ProviderTypes } from '@ew-did-registry/did-resolver-interface';
 import { EwSigner } from '@ew-did-registry/did-ethr-resolver';
-import { Revocation } from '../src/implementations/revocation';
-const { fail } = assert;
+import { RevocationOffChain } from '../src/implementations/revocationOffChain';
 const { JsonRpcProvider } = providers;
 
 describe('[REVOCATION CLAIMS]', function () {
-  const provider = new JsonRpcProvider('http://localhost:8544');
-  let revocationRegistry: Revocation;
-  let userDid : string;
-  let registryOffChain : string;
-  const user = provider.getSigner(7);
-  const userRole = "user";
-  let userAddress : string;
+  let revocationRegistry: RevocationOffChain;
+  let registryAddress : string;
+  const credential = "This is a test credential which states nothing";
   const revokerKeys = new Keys({
     privateKey: 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'
   });
@@ -25,41 +19,34 @@ describe('[REVOCATION CLAIMS]', function () {
     type: ProviderTypes.HTTP,
   };
   const revoker = EwSigner.fromPrivateKey(revokerKeys.privateKey, providerSettings);
-  
-
-  before(async () => {
-    userAddress =await user.getAddress();
-    userDid = `did:${Methods.Erc1056}:${userAddress}`;
-  });
 
   beforeEach(async () => {
-    registryOffChain = await deployRevocationRegistry();
-    revocationRegistry = new Revocation(revoker, registryOffChain, registryOffChain);
+    registryAddress = await deployRevocationRegistry();
+    revocationRegistry = new RevocationOffChain(revoker, registryAddress);
   });
 
-    it('Revoker can revoke a role', async () => {
-      await revocationRegistry.revokeOffChainRole(userRole, userDid);
-      expect (await revocationRegistry.isRoleRevoked(userRole, userDid, false)).true;
-    });
+  it('Revoker can revoke a credential', async () => {
+    expect(await revocationRegistry.revokeRole(credential)).true;
+  });
 
-    it('User DID needs to be valid', async () => {
-      try {
-        await revocationRegistry.revokeOffChainRole(userRole, "somerandomstringnotDID");
-        fail('Error was not thrown');
-      } catch (e) {
-        expect(e.message).to.equal('Invalid DID');
-      }
-      expect(await revocationRegistry.isRoleRevoked(userRole, userDid, false)).false;
-    });
+  it('A credential can be revoked multiple times', async () => {
+    expect(await revocationRegistry.revokeRole(credential)).true;
+    expect(await revocationRegistry.revokeRole(credential)).true;
+  });
 
-    it('Revoker details can be fetched for a revoked off chain role', async () => {
-      
-      await revocationRegistry.revokeOffChainRole(userRole, userDid );
-      expect(await revocationRegistry.isRoleRevoked(userRole, userDid, false)).true;
-      
-      const result = await revocationRegistry.getRevocationDetail(userRole, userDid, false);
+  it('Revoker for a credential can be fetched', async () => {
+    expect(await revocationRegistry.revokeRole(credential)).true;
+    const result = await revocationRegistry.getRevocationDetail(credential);
+    expect(result.length).to.equal(2);
+    expect(result[0][0]).equal(revokerAddress);
+  });
+
+  it('Revokers can be fetched for a credential revoked multiple times', async () => {
+      expect(await revocationRegistry.revokeRole(credential)).true;
+      expect(await revocationRegistry.revokeRole(credential)).true;
+      const result = await revocationRegistry.getRevocationDetail(credential);
       expect(result.length).to.equal(2);
-      expect(result[0]).equal(revokerAddress);
-
-    });
+      expect(result[0][0]).equal(revokerAddress);
+      expect(result[0][1]).equal(revokerAddress);
+  });
 });
