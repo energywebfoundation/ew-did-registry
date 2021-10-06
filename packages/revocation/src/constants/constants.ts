74ee511de069fb02de52be5547b4b4d46f373892
nichonien <ashish.tripathi@energyweb.org>
2021-10-06 18:22:47 +0530

    feat/EDR-40/add deployed contracts address

--- packages/revocation/src/constants/constants.ts
+++ packages/revocation/src/constants/constants.ts
@@ -1,7 +1,7 @@
 
 // Address of smart contracts on Volta
-export const RevocationRegOffChainCalims = '';
-export const RevocationRegsOnChainClaims = '';
-export const ENSRegistryAddr = '';
-export const ClaimManagerAddr = '';
-export const ERC1056Addr = '';
+export const RevocationRegOffChainAddr = '0x3A859706c2491993Cf07760bf68F40B10946fAc5';
+export const RevocationRegsOnChainAddr = '0xC8E016f86Bcbb116e5B6b6E4F74097f41826c47c';
+export const ENSRegistryAddr = '0xd7CeF70Ba7efc2035256d828d5287e2D285CD1ac';
+export const ClaimManagerAddr = '0xC3dD7ED75779b33F5Cfb709E0aB02b71fbFA3210';
+export const ERC1056Addr = '0x84d0c7284A869213CB047595d34d6044d9a7E14A';
