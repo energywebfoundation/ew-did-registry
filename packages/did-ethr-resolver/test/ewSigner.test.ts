3d433cfe1ad7a87b26ed3378e1b3c9e87e32e709
John Henderson <john.henderson@energyweb.org>
2021-10-07 10:40:07 +0200

    test(ewSigner): fromPrivateKey's pubKey needs 0x

--- packages/did-ethr-resolver/test/ewSigner.test.ts
+++ packages/did-ethr-resolver/test/ewSigner.test.ts
@@ -1,9 +1,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+import { ProviderSettings, ProviderTypes } from '@ew-did-registry/did-resolver-interface';
 import { expect } from 'chai';
 import { providers, Wallet } from 'ethers';
 import { EwSigner } from '../src/implementations';
 
 describe('[RESOLVER PACKAGE]: EWSIGNER', () => {
+  it('instantiation from privateKey should give publicKey with 0x prefix', async () => {
+    const signer = Wallet.createRandom();
+    const providerSettings: ProviderSettings = {
+      type: ProviderTypes.HTTP,
+    };
+    expect(() => EwSigner.fromPrivateKey(signer.privateKey, providerSettings).publicKey.slice(0, 2)).to.equal('0x');
+  });
+
   it('instantiation from provider without signer should throw error', async () => {
     const signer = Wallet.createRandom();
     expect(() => EwSigner.fromEthersSigner(signer, '')).to.throw();
