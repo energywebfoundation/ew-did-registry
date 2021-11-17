fbc01f13c3024c69e775e1142a9a070fba6f0730
nichonien <ashish.tripathi@energyweb.org>
2021-11-17 14:42:29 +0530

    EDR-88/add chain name and did patterns

--- packages/did/src/models/index.ts
+++ packages/did/src/models/index.ts
@@ -1,22 +1,27 @@
 /* eslint-disable no-shadow */
 export interface IDidStore {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 export enum Methods {
-    Erc725 = 'erc725',
-    Erc1056 = 'ethr',
-    Jolocom = 'jolo'
+  Erc725 = 'erc725',
+  Erc1056 = 'ethr',
+  Jolocom = 'jolo'
+}
+
+export enum Chain {
+  EWC = 'ewc',
+  VOLTA = 'volta'
 }
 
 export const DID_SCHEME_PATTERNS = {
 
-  NETWORK: /^[a-z0-9]+$/,
-  /**
-     * DID specification rule for method-specific-id
-   * DID specification rule for method-name
-   * The pattern allows an empty identifier to identify a method or did-registry
-   * See [Issue 34] {@link https://github.com/w3c/did-core/issues/34}
-     */
-  ID: /^[\w.-]*(:[\w.-]*)*$/,
+NETWORK: /^[a-z0-9]+?(:)?[a-z0-9]+?$/,
+/**
+   * DID specification rule for method-specific-id
+ * DID specification rule for method-name
+ * The pattern allows an empty identifier to identify a method or did-registry
+ * See [Issue 34] {@link https://github.com/w3c/did-core/issues/34}
+   */
+ID: /^[\w.-]*(:[\w.-]*)*$/,
 };
