96574c3b30a3fa194be0136c376ef10311a29d96
nichonien <ashish.tripathi@energyweb.org>
2021-11-23 11:16:22 +0530

    codacy changes

--- packages/proxyIdentity/migrations/2_identityManager_migrations.js
+++ packages/proxyIdentity/migrations/2_identityManager_migrations.js
@@ -1,6 +1,6 @@
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
-//needs to be updated with actual address
+// needs to be updated with actual address
 const libraryAddress = '0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB';
 const IdentityManager = artifacts.require('IdentityManager');
 
--- packages/proxyIdentity/test/truffle/upgradeContract.ts
+++ packages/proxyIdentity/test/truffle/upgradeContract.ts
@@ -1,17 +1,17 @@
-import fs from "fs";
+import fs from 'fs';
 
 const claimManagerContract = fs.readFileSync(
-  "./contracts/IdentityManager.sol",
-  "utf8",
+  './contracts/IdentityManager.sol',
+  'utf8',
 );
 
-// Creates a v0.2 of the IdentityManager contract so that it can be used as an input to the upgrade test
-// (i.e. the test needs a contract to upgrade to)
+// Creates a v0.2 of the IdentityManager contract so that it can be used
+// as an input to the upgrade test(i.e. the test needs a contract to upgrade to)
 const upgradedIdentityManagerContract = claimManagerContract
   .replace(`contract IdentityManager is `, `contract IdentityManagerUpgradeTest is `)
   .replace(`return "v0.1";`, `return "v0.2";`);
 
 fs.writeFileSync(
-  "./contracts/IdentityManagerUpgradeTest.sol",
+  './contracts/IdentityManagerUpgradeTest.sol',
   upgradedIdentityManagerContract,
 );
