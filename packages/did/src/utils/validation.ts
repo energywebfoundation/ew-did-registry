90b1c59c76ad7e0528e969723b960fe0a4d1c9d8
Jakub Sydor <sydorjakub@gmail.com>
2022-02-17 18:16:38 +0100

    feat: update `isValidErc1056` to accept DID address with chain name
    `isValidErc1056` assuming a valid DID address with and without
    method specific id

--- packages/did/src/utils/validation.ts
+++ packages/did/src/utils/validation.ts
@@ -1,12 +1,17 @@
 import { Methods, Chain } from '../models';
 
 export const ethAddrPattern = '0x[A-Fa-f0-9]{40}';
-export const DIDPattern = `^did:${Methods.Erc1056}:(${ethAddrPattern})$`;
+export const DIDPattern = `^did:${Methods.Erc1056}:([A-Za-z0-9]+:)?(${ethAddrPattern})$`;
+export const DIDPatternMainnet = `^did:${Methods.Erc1056}:(${ethAddrPattern})$`;
 export const DIDPatternEWC = `^did:${Methods.Erc1056}:${Chain.EWC}:(${ethAddrPattern})$`;
 export const DIDPatternVOLTA = `^did:${Methods.Erc1056}:${Chain.VOLTA}:(${ethAddrPattern})$`;
 
 export function isValidErc1056(did: string): boolean {
   return new RegExp(DIDPattern).test(did);
+}
+
+export function isValidErc1056Mainnet(did: string): boolean {
+  return new RegExp(DIDPatternMainnet).test(did);
 }
 
 export function isValidErc1056EWC(did: string): boolean {
