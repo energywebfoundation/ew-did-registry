8dbbc1839320862e83bf196371e85c36829783c6
nichonien <ashish.tripathi@energyweb.org>
2022-11-10 16:25:27 +0530

    docs: update comment

--- packages/proxyIdentity/migrations/2_identityManager_migrations.js
+++ packages/proxyIdentity/migrations/2_identityManager_migrations.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
-// needs to be updated with actual address
+// Deployed OfferableIdentity contract address
 const libraryAddress = '0x4c392f00a17B1b77aB059E6dc9ccE91F74113c66';
 // eslint-disable-next-line no-undef
 const IdentityManager = artifacts.require('IdentityManager');
