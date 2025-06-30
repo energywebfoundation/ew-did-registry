d4ffdad7d29a04ba84d66be8fcf3d248cd7218e6
azam-ismail <azam.ismail@energyweb.org>
2025-06-30 09:49:32 +0800

    feat: ssi hub api store integration deploy

---
+++ packages/did-ssi-hub-store/README.md
@@ -0,0 +1,9 @@
+# @ew-did-registry/did-ssi-hub-store
+
+This package provides SSI-HUB S3-based implementation of the IDidStore
+
+## Structure
+
+- dist - contains compiled javascript files
+- src - contains source typescript files
+- test - contains unit test files
---
+++ packages/did-ssi-hub-store/src/didStore.ts
@@ -0,0 +1,42 @@
+import { IDidStore } from "@ew-did-registry/did-store-interface";
+import { CacheClient, CacheClientConfig } from "./cacheCache";
+
+/**
+ * Implements decentralized storage in S3 Bucket
+ */
+export class DidStore implements IDidStore {
+  private client: CacheClient;
+
+  constructor(cacheConfig: CacheClientConfig) {
+    this.client = new CacheClient(cacheConfig);
+  }
+
+  /**
+   * @param claim stringified content
+   */
+  async save(claim: string): Promise<string> {
+    const res = await this.client.post("/s3", { data: claim });
+    return res.data;
+  }
+
+  /**
+   * Looks up content identified by `cid`. If no content found during `timeout`, then `ContentNotFound` error is thrown.
+   * @param cid CID of the content
+   * @param timeout time limit for getting response, milliseconds
+   * @returns stringified content
+   */
+  async get(uri: string): Promise<string> {
+    const res = await this.client.get(`/s3/${uri}`);
+    return res.data;
+  }
+
+
+  async delete(uri: string): Promise<boolean> {
+    try {
+      return false;
+    } catch (err: any) {
+      if (err.name === "NoSuchKey") return false;
+      throw err;
+    }
+  }
+}
